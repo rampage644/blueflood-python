202d9c0f5a96aeca7c308d444951aee7ff431907
Sergei Turukin <rampage644@gmail.com>
2014-12-03 12:26:26 +0200

    Rename modules to replace hyphen wit underscore

--- blueflood_python/blueflood.py
+++ blueflood_python/blueflood.py
@@ -17,11 +17,13 @@ def _get_metrics_query_url(host, port, scheme, tenantId,
 
 
 # TODO add retrieve method
+# TODO do something with schema (parametrize, include in host)
 class BluefloodEndpoint():
 
-    def __init__(self, host='localhost', port='19000', tenant='tenant-id'):
+    def __init__(self, host='localhost', ingest_port='19000', retrieve_port='20000', tenant='tenant-id'):
+        self.ingest_port = ingest_port
+        self.retrieve_port = retrieve_port
         self.host = host
-        self.port = port
         self.tenant = tenant
 
     def ingest(self, metric_name, time, value, ttl):
@@ -39,8 +41,17 @@ class BluefloodEndpoint():
             "metricValue": v,
             "metricName": metric_name
         } for t,v in zip(time, value)]
-        # print (_get_metrics_url(self.host, self.port, 'http', 'tenant-id'))
+        print (_get_metrics_url(self.host, self.ingest_port, 'http', 'tenant-id'))
         # print (json.dumps(data))
-        r = urllib2.urlopen(_get_metrics_url(self.host, self.port, 'http', self.tenant),
+        r = urllib2.urlopen(_get_metrics_url(self.host, self.ingest_port, 'http', self.tenant),
                             data=json.dumps(data))
         return r.read()
+
+    def retrieve(self, metric_name, start, to, points):
+        print (_get_metrics_query_url(self.host, self.retrieve_port, 'http', self.tenant,
+                                                   metric_name, start, to, points))
+        r = urllib2.urlopen(_get_metrics_query_url(self.host, self.retrieve_port, 'http', self.tenant,
+                                                   metric_name, start, to, points))
+        response = r.read()
+        print (response)
+        return json.loads(response)
