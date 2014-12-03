c610afc2be6d832e01b0f0a07eeaa992bd07642f
Sergei Turukin <rampage644@gmail.com>
2014-12-03 12:49:26 +0200

    Switch from `unittest` to `pytest`

--- test/test_blueflood_python.py
+++ test/test_blueflood_python.py
@@ -1,43 +1,49 @@
 #!/usr/bin/env python
 
 from blueflood_python.blueflood import BluefloodEndpoint
-import unittest
+import pytest
 
 
-class BlueFloodTestCase(unittest.TestCase):
+@pytest.fixture
+def setup():
+    return BluefloodEndpoint()
 
-    def setUp(self):
-        self.endpoint = BluefloodEndpoint()
+def testSingleIngest(setup):
+    endpoint = setup
+    name = 'example.metric.one'
+    ttl = 10
+    assert endpoint.ingest(name, 1376509892612, 50, ttl) == ''
 
-    def testSingleIngest(self):
-        name = 'example.metric.one'
-        ttl = 10
-        self.assertEqual(
-            self.endpoint.ingest(name, 1376509892612, 50, ttl), '')
+def testListIngest(setup):
+    endpoint = setup
+    name = 'example.metric.one'
+    ttl = 10
+    assert endpoint.ingest(name,
+                           [1376509892612, 1376509892613, 1376509892614], [50, 51, 52], ttl) == ''
+    with pytest.raises(Exception):
+        endpoint.ingest(
+            name, [1376509892612, 1376509892613, 1376509892614], 50, ttl)
 
-    def testListIngest(self):
-        name = 'example.metric.one'
-        ttl = 10
-        self.assertEqual(self.endpoint.ingest(
-            name, [1376509892612, 1376509892613, 1376509892614], [50, 51, 52], ttl), '')
-        with self.assertRaises(Exception):
-            self.endpoint.ingest(
-                name, [1376509892612, 1376509892613, 1376509892614], 50, ttl)
+def testWrongEndpointConfig(setup):
+    self.endpoint.ingest_port = '19001'
+    with pytest.raises(HTTPError):
+        assert self.endpoint.ingest(name, 1376509892612, 50, ttl) == ''
 
-    def testRetrieve(self):
-        name = 'example.metric.retrieve'
-        ttl = 10
-        time = 1376509892612
-        value = 50
-        # insert first
-        self.assertEqual(self.endpoint.ingest(name, time, value, ttl), '')
-        # range is 0-time, 200 points
-        data = self.endpoint.retrieve(name, 0, time + 10, 200)
-        self.assertNotEquals(len(data), 0)
-        self.assertNotEquals(len(data['values']), 0)
-        self.assertEquals(data['values'][0]['numPoints'], 1)
-        self.assertEquals(data['values'][0]['average'], value)
+def testRetrieve(setup):
+    endpoint = setup
+    name = 'example.metric.retrieve'
+    ttl = 10
+    time = 1376509892612
+    value = 50
+    # insert first
+    assert endpoint.ingest(name, time, value, ttl) == ''
+    # range is 0-time, 200 points
+    data = endpoint.retrieve(name, 0, time + 10, 200)
+    assert len(data) != 0
+    assert len(data['values']) != 0
+    assert data['values'][0]['numPoints'] == 1
+    assert data['values'][0]['average'] == value
 
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
